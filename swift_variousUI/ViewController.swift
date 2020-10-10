44d64dbb171617a3505661ff88912a3f8a60c563
神代泰宏 <yasuhiro79367s@hotmail.co.jp>
2020-10-10 22:08:23 +0900

    first commit

---
+++ swift_variousUI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swift_variousUI
+//
+//  Created by kuma on 2020/10/10.
+//  Copyright © 2020 kuma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
