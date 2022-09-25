2e01ef6740476b2b9fa4bab8dbeb8ae891f11002
神代泰宏 <yasuhiro79367s@hotmail.co.jp>
2022-09-25 11:31:20 +0900

    add Perchement Librry

--- Podfile
+++ Podfile
@@ -2,10 +2,9 @@
 # platform :ios, '9.0'
 
 target 'swift_variousUI' do
-  # Comment the next line if you don't want to use dynamic frameworks
   use_frameworks!
 
-  # Pods for swift_variousUI
+  pod 'Parchment'
 
   target 'swift_variousUITests' do
     inherit! :search_paths
--- Pods/Target Support Files/Pods-swift_variousUI/Pods-swift_variousUI-frameworks.sh
+++ Pods/Target Support Files/Pods-swift_variousUI/Pods-swift_variousUI-frameworks.sh
@@ -197,10 +197,10 @@ if [ -r "${ARTIFACT_LIST_FILE}" ]; then
 fi
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/SwipeMenuViewController/SwipeMenuViewController.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Parchment/Parchment.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/SwipeMenuViewController/SwipeMenuViewController.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/Parchment/Parchment.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
