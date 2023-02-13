0d62754e20e980ae721cf1a03aa8f81660ac753c
神代泰宏 <yasuhiro79367s@hotmail.co.jp>
2023-02-14 00:32:10 +0900

    add Extension to hide keyboard in viewController

---
+++ swift_variousUI/Extension/UIViewController+Keyboard.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+extension UIViewController {
+    func hideKeyboardOnTap() {
+        let gesture = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        view.addGestureRecognizer(gesture)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
